2a0d1c2a0867b9a78ad5fccbcfe1b5b0b32d60fd
985892345 <guo985892345@foxmail.com>
2022-08-12 14:02:21 +0800

    :sparkles: 分离 main 源集，打算单独做成依赖包

--- README.md
+++ README.md
@@ -4,3 +4,21 @@
 
 > 卷娘可爱捏
 
+## 如何参与？
+因为本项目使用了非官方的 mirai 热修插件，
+详细用法可以参考该项目：[mirai-hotfix](https://github.com/985892345/mirai-hotfix)，
+本文只介绍如何快速参与，以下步骤分为 [Jvm 平台](#Jvm 平台)和 [Js 平台](#Js 平台)
+
+### Jvm 平台
+#### 基础步骤
+1、首先需要你有一定的 java 或 kotlin 基础，并了解过 [mirai](https://github.com/mamoe/mirai) 的写法
+> 若不是很了解的话，建议先看一下 mirai 的官方文档：https://github.com/mamoe/mirai  
+> 如果你只是想简单提个 pr，加个彩蛋，可以直接看官方的 api 文档：https://github.com/mamoe/mirai/blob/dev/docs/CoreAPI.md
+
+2、然后 clone 本项目，并在 idea 中编译起来  
+
+如果你已成功编译，那么就可以开始开发了
+
+### Js 平台
+
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
-    java
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-    
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+    id("net.mamoe.mirai-console") version "2.12.1"
     // mirai 热修插件 https://github.com/985892345/mirai-hotfix
-    id("io.github.985892345.mirai-hotfix") version "1.1"
+    id("io.github.985892345.mirai-hotfix") version "1.2"
 }
 
 group = "team.redrock"
@@ -14,21 +12,4 @@ version = "1.0-SNAPSHOT"
 repositories {
     maven("https://maven.aliyun.com/repository/public")
     mavenCentral()
-}
-
-dependencies {
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
-    compileOnly("com.google.code.gson:gson:2.9.0")
-    compileOnly("net.mamoe:mirai-core:2.11.1")
-    compileOnly("net.mamoe:mirai-console:2.11.1")
-    compileOnly(kotlin("stdlib"))
-    testImplementation("net.mamoe:mirai-console-terminal:2.11.1")
-}
-
-hotfix {
-    createHotfix("Interceptors")
-    createHotfix("ServiceCleanMember")
-    createHotfix("ServiceDirtyWord")
-    createHotfix("ServiceJoin")
-    createHotfix("ServiceRepeatMessage")
 }
