71a17584e96bfc7028945f293442561d669e7d65
985892345 <guo985892345@foxmail.com>
2022-08-12 16:59:42 +0800

    :construction: 将 mirai-hotfix 改为 api 依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     kotlin("plugin.serialization") version kotlinVersion
     id("net.mamoe.mirai-console") version "2.12.1"
     // mirai 热修插件 https://github.com/985892345/mirai-hotfix
-    id("io.github.985892345.mirai-hotfix") version "1.3"
     `maven-publish`
 }
 
@@ -14,6 +13,10 @@ version = "base-0.1-alpha1"
 repositories {
     maven("https://maven.aliyun.com/repository/public")
     mavenCentral()
+}
+
+dependencies {
+    api("com.github.985892345:mirai-hotfix:1.3")
 }
 
 // 发布带有 mirai-hotfix 的依赖
